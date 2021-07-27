4b9f738cb0407c70ced1805c08c4185c0ae85aa6
LoneSheep <yuarisarham@gmail.com>
2021-07-27 16:49:39 +0700

    Digital Corner with Database

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
+const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar_menu');
 const navLogo = document.querySelector('#navbar_logo');
+
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
 
 menu.addEventListener('click', mobileMenu);
 
@@ -33,3 +39,14 @@ const highlightMenu = () => {
 
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
+
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
